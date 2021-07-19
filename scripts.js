d0a51a02686723a16987d4161e26dd8f58777dec
Squagilli <cotrupeconnor@gmail.com>
2021-07-19 16:15:34 +0200

    added html and css and basic math functions

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let numberOne = 2;
+let numberTwo = 3;
+
+
+function sum(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+function subract(numberOne, numberTwo) {
+    return numberOne - numberTwo;
+}
+function multiply(numberOne, numberTwo) {
+    return numberOne * numberTwo;
+}
+function divide(numberOne, numberTwo) {
+    return numberOne / numberTwo;
+}
+
+console.log(sum(2, 3));
+console.log(subract(9, 3));
+console.log(multiply(9, 3));
+console.log(divide(9, 3));
+
+
