6666d62c845e8e1fd6ba8f1ae70ea5ae27b8637c
Squagilli <cotrupeconnor@gmail.com>
2021-07-26 12:17:08 +0200

    constructing calculator logic

--- scripts.js
+++ scripts.js
@@ -1,34 +1,53 @@
-// HTML ITEMS
+// Calc HTML function buttons
 const calcDisplay = document.querySelector('.calc-input');
-
-const one = document.querySelector('.one');
-
 const clear = document.querySelector('.clear');
+const functionDecimal = document.querySelector('.dot');
+const functionAdd = document.querySelector('.add');
+const functionSubtract = document.querySelector('.subtract');
+const functionMultiply = document.querySelector('.multiply');
+const functionDivide = document.querySelector('.divide');
+const functionRemainder = document.querySelector('.percent');
+
+// HTML number buttons
+const zero = document.querySelector('.zero');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+
+// Number Storage
+const numberZero = '0';
+const numberOne = '1';
+const numberTwo = '2';
+const numberThree = '3';
+const numberFour = '4';
+const numberFive = '5';
+const numberSix = '6';
+const numberSeven = '7';
+const numberEight = '8';
+const numberNine = '9';
+
+const numberDec = '.';
+
 
 
 // CALULATOR LOGIC
-//let numberOne = prompt('first number');
-//let numberTwo = prompt('second number');
-//const parseOne = parseInt(numberOne, 10);
-//const parseTwo = parseInt(numberTwo, 10);
 
-function operate(parseOne, parseTwo) {
-    let operation = prompt('What u wanna do?');
-    if (operation === 'add') {
-        alert(sum(parseOne, parseTwo));
-    }
-    else if (operation === 'subtract') {
-        alert(subract(parseOne, parseTwo));
-    }
-    else if (operation === 'multiply') {
-        alert(multiply(parseOne, parseTwo));
-    }
-    else if (operation === 'divide') {
-        alert(divide(parseOne, parseTwo));
-    }
+//operate
+
+// user inputs first number (number buttons event listeners)
+
+// number is stored as num1
+// user selects operator
+// user inputs second number
+// display result
 
 
-}
 
 function sum(parseOne, parseTwo) {
     return parseOne + parseTwo;
@@ -43,13 +62,65 @@ function divide(parseOne, parseTwo) {
     return parseOne / parseTwo;
 }
 
-//operate(parseOne, parseTwo);
 
 
-// event listeners
+// Functional buttons event listeners
 clear.addEventListener('click', function() {
-    calcDisplay.textContent = '';
+    calcDisplay.innerHTML = '';
+});
+functionDecimal.addEventListener('click', function() {
+    calcDisplay.append(numberDec);
+});
+functionAdd.addEventListener('click', function() {
+    let firstNumber = calcDisplay.innerHTML;
+    console.log(firstNumber);
+    calcDisplay.innerHTML = '';
+});
+functionSubtract.addEventListener('click', function() {
+    let firstNumber = calcDisplay.innerHTML;
+    console.log(firstNumber);
+    calcDisplay.innerHTML = '';
+});
+functionMultiply.addEventListener('click', function() {
+    let firstNumber = calcDisplay.innerHTML;
+    console.log(firstNumber);
+    calcDisplay.innerHTML = '';
+});
+functionDivide.addEventListener('click', function() {
+    let firstNumber = calcDisplay.innerHTML;
+    console.log(firstNumber);
+    calcDisplay.innerHTML = '';
+});
+
+
+// Number buttons event listeners
+zero.addEventListener('click', function() {
+    calcDisplay.append(numberZero);
 });
 one.addEventListener('click', function() {
-    calcDisplay.textContent = '1';
+    calcDisplay.append(numberOne);
+});
+two.addEventListener('click', function() {
+    calcDisplay.append(numberTwo);
+});
+three.addEventListener('click', function() {
+    calcDisplay.append(numberThree);
+});
+four.addEventListener('click', function() {
+    calcDisplay.append(numberFour);
+});
+five.addEventListener('click', function() {
+    calcDisplay.append(numberFive);
+});
+six.addEventListener('click', function() {
+    calcDisplay.append(numberSix);
+});
+seven.addEventListener('click', function() {
+    calcDisplay.append(numberSeven);
+});
+eight.addEventListener('click', function() {
+    calcDisplay.append(numberEight);
+});
+nine.addEventListener('click', function() {
+    calcDisplay.append(numberNine);
 });
